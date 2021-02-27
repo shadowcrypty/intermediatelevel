82cdd2352ad5f1a52549103ebd0074167703e695
DESKTOP-HSGUA6V\Anarchy <ziyanamli@gmail.com>
2021-02-27 18:50:47 +0300

    created rigidbody controller

---
+++ Assets/Scripts/rigidbodycontroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lesssons
+{
+
+
+    public class rigidbodycontroller : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody rigidbody;
+        [SerializeField] private rigidbodysettings rigidbodysettings;
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rigidbody.AddForce(rigidbodysettings.JumpForce,ForceMode.Impulse);
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/rigidbodysettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Lesssons
+{
+
+    [CreateAssetMenu(menuName = "Lessons/RigidbodySettings")]
+    public class rigidbodysettings : ScriptableObject
+    {
+        [SerializeField]private Vector3 _jumpforce;
+        public Vector3 JumpForce { get { return _jumpforce; } }
+
+    }
+}

17e5011d6f8be1647dec1d5f03789c71964e216d
DESKTOP-HSGUA6V\Anarchy <ziyanamli@gmail.com>
2021-02-27 18:49:35 +0300

    created camera control scripts

---
+++ Assets/TopDown_Shooter/Scripts/Camera/Cameracontroller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+
+
+    public class Cameracontroller : MonoBehaviour
+    {
+        [SerializeField] private Camerasettings _camerasettings;
+        [SerializeField] private Transform _targetTransform;
+        [SerializeField] private Transform _cameraTransform;
+
+        private void Update()
+        {
+            _cameraTransform.rotation = Quaternion.Lerp(_cameraTransform.rotation, Quaternion.LookRotation(_targetTransform.position - _cameraTransform.position), Time.deltaTime * _camerasettings.LerpSpeed);
+        }
+
+    }
+}
---
+++ Assets/TopDown_Shooter/Scripts/Camera/Camerasettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+
+    [CreateAssetMenu(menuName = "TopDownShooter/Camera/CameraSettings")]
+    public class Camerasettings : ScriptableObject
+    {
+        [SerializeField] public float _lerpSpeed;
+        public float LerpSpeed { get { return _lerpSpeed; } }
+    }
+}

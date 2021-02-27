1b1974cf22e3169e5837d871cdd5b2860423432b
DESKTOP-HSGUA6V\Anarchy <ziyanamli@gmail.com>
2021-02-27 15:01:51 +0300

    +Created: rotatescript +Created: rotatescript values saving scriptable object +Created: test cube

---
+++ Assets/Scripts/rotateobject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Lesssons
+{
+
+
+    public class rotateobject : MonoBehaviour
+    {
+        
+        [SerializeField] private rotatesettings rotatesettings;
+        
+
+        private void Update()
+        {
+            transform.Rotate(Vector3.up, rotatesettings.Speed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/rotatesettings.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lesssons
+{
+
+    [CreateAssetMenu(menuName ="Lessons/RotateSettings")]
+    
+    public class rotatesettings : ScriptableObject
+    {
+        [SerializeField] public float _speed;
+        public float Speed { get { return _speed; }  }
+
+
+    }
+}

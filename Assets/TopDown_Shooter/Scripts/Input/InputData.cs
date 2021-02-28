8b455d4c4c83554b93115ae61e44108dc579eb8f
DESKTOP-HSGUA6V\Anarchy <ziyanamli@gmail.com>
2021-02-28 12:45:52 +0300

    created:input system

---
+++ Assets/TopDown_Shooter/Scripts/Input/InputData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TopDownShooter.UserInput
+{
+
+    [CreateAssetMenu(menuName = "TopDownShooter/Input/InputData")]
+    public class InputData : ScriptableObject
+    {
+        public float horizontal;
+        public float vertical;
+
+    }
+}
---
+++ Assets/TopDown_Shooter/Scripts/Input/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.UserInput
+{
+
+public class InputManager : MonoBehaviour
+{
+        [SerializeField] private InputData _inputData;
+
+        void Update()
+        {
+            _inputData.horizontal = Input.GetAxis("Horizontal");
+
+            _inputData.vertical= Input.GetAxis("Vertical");
+        }
+}
+
+}

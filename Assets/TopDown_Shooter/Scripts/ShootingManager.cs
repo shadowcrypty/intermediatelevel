0b03d7ebbedff77e524d10e51b988c1b09f3585a
DESKTOP-HSGUA6V\Anarchy <ziyanamli@gmail.com>
2021-02-28 18:29:29 +0300

    created:basic ray hit shoot

---
+++ Assets/TopDown_Shooter/Scripts/ShootingManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.Shooting
+{
+
+
+    public class ShootingManager : MonoBehaviour
+    {
+        public void Shoot(Vector3 from,Vector3 direction)
+        {
+            RaycastHit hit;
+            bool rayHit = Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity);
+            if (rayHit)
+            {
+                Debug.Log(hit.collider.name);
+
+            }
+        }
+    }
+}

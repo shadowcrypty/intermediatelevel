844eee23ec3efb41db2cbc63d54f9acba74a33ee
DESKTOP-HSGUA6V\Anarchy <ziyanamli@gmail.com>
2021-02-28 12:46:40 +0300

    created:basic movment system

---
+++ Assets/TopDown_Shooter/Scripts/PlayerMovment/PlayerMovmentController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TopDownShooter.UserInput;
+using UnityEngine;
+
+namespace TopDownShooter.PlayerMovment
+{
+
+
+    public class PlayerMovmentController : MonoBehaviour
+    {
+        [SerializeField] private InputData _inputData;
+        [SerializeField] private Rigidbody rigidbody;
+        [SerializeField] private PlayerMovmentSettings _playerMovmentSettings;
+
+        private void Update()
+        {
+            rigidbody.MovePosition(rigidbody.position+rigidbody.transform.right * _inputData.horizontal*_playerMovmentSettings.horizontalSpeed);
+            rigidbody.MovePosition(rigidbody.position+rigidbody.transform.forward * _inputData.vertical*_playerMovmentSettings.verticalSpeed);
+        }
+    }
+}
---
+++ Assets/TopDown_Shooter/Scripts/PlayerMovment/PlayerMovmentSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.PlayerMovment
+{
+
+
+    [CreateAssetMenu(menuName = "TopDownShooter/Player/MovmentSettings")]
+    public class PlayerMovmentSettings : ScriptableObject
+    {
+        public float horizontalSpeed=5;
+        public float verticalSpeed=5;
+    }
+}
